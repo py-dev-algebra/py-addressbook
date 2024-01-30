54d5bcf90611d3602ce0bdbf02f309607e452386
Krunoslav Lujic <python.developer.edukacija@gmail.com>
2024-01-30 21:13:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ id_contact = 15
 contact = get_contact(id_contact)
 if contact != None:
     print(contact)
+    print(contact.id)
 else:
     print(f'Ne postoji trazeni kontakt s ID: {id_contact} u bazi!')
